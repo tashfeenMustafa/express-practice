eecb99bcc4d2387fa9551a1b59e1c84ab848bc9f
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2022-07-27 03:38:36 +0600

    fifth commit

--- app.js
+++ app.js
@@ -1,15 +1,63 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// setup static and middleware
-app.use(express.static('./public'));
+const { products } = require('./data');
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found');
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, image, name } = product;
+    return { id, image, name };
+  });
+  res.json(newProducts);
+});
+
+app.get('/api/v1/query', (req, res) => {
+  console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    res
+      .status(200)
+      .json({
+        success: true,
+        data: []
+      });
+  }
+
+  res.status(200).json(sortedProducts);
+});
+
+app.get('/api/products/:productID', (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find((product) => Number(productID) === product.id);
+
+  if (!singleProduct) {
+    return res.status(404).send('Product does not exist.')
+  }
+
+  res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('hello');
 });
 
 app.listen(5000, () => {
-  console.log(`Server is listening on port 5000`);
-})
+  console.log('Server is listening on port 5000');
+});
