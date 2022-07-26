136609afc53b2de6dc7eb661c8788d34aad9025a
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2022-07-27 02:18:53 +0600

    fourth commit

--- app.js
+++ app.js
@@ -1,47 +1,15 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const path = require('path');
 
-// get all files
-const homePage = readFileSync('./index.html');
-const homeStyles = readFileSync('./styles.css');
-const homeImage = readFileSync('./logo.svg');
-const homeLogic = readFileSync('./browser-app.js');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
+// setup static and middleware
+app.use(express.static('./public'));
 
-  if (url === '/') {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write(homePage);
-    res.end();
-  }
-  else if (url === '/about') {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write('<h1>about</h1>');
-    res.end();
-  }
-  else if (url === '/styles.css') {
-    res.writeHead(200, {'content-type': 'text/css'});
-    res.write(homeStyles);
-    res.end();
-  }
-  else if (url === '/logo.svg') {
-    res.writeHead(200, {'content-type': 'image/svg+xml'});
-    res.write(homeImage);
-    res.end();
-  }
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, {'content-type': 'text/javascript'});
-    res.write(homeLogic);
-    res.end();
-  }
-  else {
-    res.writeHead(404, {'content-type': 'text/html'});
-    res.write('<h1>404 not found</h1>');
-    res.end();
-  }
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found');
 });
 
-server.listen(5000);
-
-// http://localhost:5000/
+app.listen(5000, () => {
+  console.log(`Server is listening on port 5000`);
+})
