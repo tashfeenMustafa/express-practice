828ba5c6d424f799757712793c9a673c5f51cda0
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2022-07-28 01:35:53 +0600

    middleware

--- app.js
+++ app.js
@@ -1,61 +1,36 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const logger = require('./logger');
+const authorize = require('./authorize');
 
-const { products } = require('./data');
+// req => middleware => res
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+// 1. use vs route
+// 2. options - our own / express / third party
+
+// app.use([logger, authorize]);
+
+//app.use(express.static('./public'));
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => { 
+  res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
 });
 
 app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, image, name } = product;
-    return { id, image, name };
-  });
-  res.json(newProducts);
+
+  res.send('Home');
 });
 
-app.get('/api/v1/query', (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    res
-      .status(200)
-      .json({
-        success: true,
-        data: []
-      });
-  }
-
-  res.status(200).json(sortedProducts);
-});
-
-app.get('/api/products/:productID', (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => Number(productID) === product.id);
-
-  if (!singleProduct) {
-    return res.status(404).send('Product does not exist.')
-  }
-
-  res.json(singleProduct);
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params);
-  res.send('hello');
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('Items');
 });
 
 app.listen(5000, () => {
